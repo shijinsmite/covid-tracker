73b51cb466f388555ca5cf50d23ffe80eb057e76
shijin <vihar57@gmail.com>
2020-08-12 14:32:19 +0530

    initial commit

---
+++ src/Infobox.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Infobox.css';
+import { Card, CardContent, Typography } from '@material-ui/core';
+
+const Infobox = ({ title, cases, total }) => {
+  return (
+    <div>
+      <Card>
+        <CardContent>
+            
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default Infobox;
